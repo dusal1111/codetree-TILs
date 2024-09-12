cd291690a140b777a70f902668db3cd6733510ed
dusal1111 <147612119+dusal1111@users.noreply.github.com>
2024-09-13 07:51:07 +0900

    Title: 자연수면 출력, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240913/자연수면 출력/output-only-natural-number.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b;
+    cin >> a >> b;
+    if (a>0){
+        while (b--){
+            cout << a;
+        }
+    }
+    else cout << 0;
+    return 0;
+}
