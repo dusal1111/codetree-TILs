b68ce8fa223d2879e8563f5c8419fdf8c5fe29b0
dusal1111 <147612119+dusal1111@users.noreply.github.com>
2024-09-20 04:57:40 +0900

    Title: 별표 출력하기 13, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240920/별표 출력하기 13/print-start-13.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    for (int i=1; i<=n; i++){
+        if (i%2!=0){
+            for (int j=0; j<n-(i/2); j++){
+                cout << "* ";
+            }
+        }
+        else {
+            for (int j=0; j<i/2; j++){
+                cout << "* ";
+            }
+        }
+        cout << "\n";
+    }
+
+    for (int i=n; i>0; i--){
+        if (i%2!=0){
+            for (int j=0; j<n-(i/2); j++){
+                cout << "* ";
+            }
+        }
+        else {
+            for (int j=0; j<i/2; j++){
+                cout << "* ";
+            }
+        }
+        cout << "\n";
+    }
+    return 0;
+}
