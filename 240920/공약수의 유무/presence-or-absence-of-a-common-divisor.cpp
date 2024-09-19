83e3276c8b699d8b0f224536b125a3a396a08aab
dusal1111 <147612119+dusal1111@users.noreply.github.com>
2024-09-20 04:01:00 +0900

    Title: 공약수의 유무, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240920/공약수의 유무/presence-or-absence-of-a-common-divisor.cpp
+++ 240920/공약수의 유무/presence-or-absence-of-a-common-divisor.cpp
@@ -6,7 +6,7 @@ int main() {
     cin >> a >> b;
     int exist=0;
     while (a<=b){
-        if(1920%a==0 || 2880%a==0) {
+        if(1920%a==0 && 2880%a==0) {
             exist=1;
             break;
         }
