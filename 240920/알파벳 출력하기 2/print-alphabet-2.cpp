8966690b0a733be76902fb4cc3e574d87315453c
dusal1111 <147612119+dusal1111@users.noreply.github.com>
2024-09-20 05:31:19 +0900

    Title: 알파벳 출력하기 2, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240920/알파벳 출력하기 2/print-alphabet-2.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    char a='A';
+    for (int i=0; i<n; i++){
+        for(int j=0; j<n; j++){
+            if (j>=i) {
+                if (a=='Z') a='A';
+                cout << a << " ";
+                a++;
+            }
+            else cout << "  ";
+        }
+        cout << endl;
+    }
+    return 0;
+}
