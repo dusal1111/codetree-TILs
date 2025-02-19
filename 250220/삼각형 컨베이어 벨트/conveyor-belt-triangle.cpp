73a403b0dd077c2ab952b54b3e1e5775127cd2b4
dusal1111 <147612119+dusal1111@users.noreply.github.com>
2025-02-20 02:31:18 +0900

    Title: 삼각형 컨베이어 벨트, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 250220/삼각형 컨베이어 벨트/conveyor-belt-triangle.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int n, t;
+int arr[400];
+
+int main() {
+    cin >> n >> t;
+
+    for (int i = 0; i < 3*n; i++) cin >> arr[i];
+
+    int idx = ((3*n) - (t % (3*n))) % (3*n);
+
+    for(int i=0; i<3*n; i++){
+        if (i==n||i==2*n) cout << "\n";
+        cout << arr[(idx+i)%(3*n)] << " ";
+    }
+
+    return 0;
+}
