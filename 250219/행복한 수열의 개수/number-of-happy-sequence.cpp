271f990746849f09e058eb77f0cfa790ff32d721
dusal1111 <147612119+dusal1111@users.noreply.github.com>
2025-02-19 19:45:18 +0900

    Title: 행복한 수열의 개수, Time: 7ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 250219/행복한 수열의 개수/number-of-happy-sequence.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+
+using namespace std;
+
+int n, m;
+int grid[100][100];
+
+int main() {
+    cin >> n >> m;
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            cin >> grid[i][j];
+        }
+    }
+
+    int happy = 0;
+    // Write your code here!
+    for (int i = 0; i < n; i++) {
+        int cnt = 0;
+        for (int j = 1; j < n; j++) {
+            if (grid[i][j-1] == grid[i][j]) cnt++;
+            if (cnt >= m-1) {
+                happy++;
+                break;
+            }
+        }
+    }
+    for (int i = 0; i < n; i++) {
+        int cnt = 0;
+        for (int j = 1; j < n; j++) {
+            if (grid[j-1][i] == grid[j][i]) cnt++;
+            if (cnt >= m-1) {
+                happy++;
+                break;
+            }
+        }
+    }
+
+    cout << happy;
+    return 0;
+}
